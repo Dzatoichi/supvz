cc981d8e30c9a57be7605db2d35fd1e0e2b286d9
Danila Kashtanov <kashtanov2004@internet.ru>
2025-09-01 12:44:10 +0700

    Fix: resolve merge conflicts with dev

--- auth_service/src/main.py
+++ auth_service/src/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import asyncio
+from auth_service.src.dao.usersDAO import UsersDAO
 
 app = FastAPI()
 
@@ -11,3 +13,11 @@ def health():
 @app.get("/")
 def root():
     return {"message": "Auth Service Running!"}
+
+async def main():
+    user_dao = UsersDAO()
+    user = await user_dao.get_by_id(1)
+    print(await user_dao.get_user_by_email(user.email+"123"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
