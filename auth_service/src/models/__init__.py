a42cae917bde57b872df372ad68e6cb974061908
vladbily <vladbilyk2005@gmail.com>
2025-09-05 20:26:28 +0700

    add decorator for exceptions

--- auth_service/src/models/__init__.py
+++ auth_service/src/models/__init__.py
@@ -1,7 +1,5 @@
-__all__ = ["Users", "UsersRoleEnum", "PVZs", "PVZWorkers", "RefreshTokens", "AccessTokens", "StatefulTokens"]
+__all__ = ["Users", "UsersRoleEnum", "RefreshTokens", "AccessTokens", "StatefulTokens"]
 
-from src.models.pvzs.PVZ_workers import PVZWorkers
-from src.models.pvzs.PVZs import PVZs
 from src.models.tokens.access_tokens import AccessTokens
 from src.models.tokens.refresh_tokens import RefreshTokens
 from src.models.tokens.stateful_tokens import StatefulTokens
