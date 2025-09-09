f8e0a225af9a3a8fce66ca4acbce5d8162b78d1e
ilya <tor-yo@ya.ru>
2025-09-09 14:45:28 +0700

    add global exceprion_handlers

--- auth_service/src/utils/exceptions.py
+++ auth_service/src/utils/exceptions.py
@@ -1,3 +1,46 @@
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import HTTPException, RequestValidationError
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def setup_exception_handlers(app: FastAPI):
+    @app.exception_handler(HTTPException)
+    async def http_exception_handler(request: Request, exc: HTTPException):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"error": "http_error", "detail": exc.detail, "path": request.url.path},
+        )
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={
+                "error": "validation_error",
+                "detail": exc.errors(),
+                "body": exc.body,
+                "path": request.url.path,
+            },
+        )
+
+    @app.exception_handler(SQLAlchemyError)
+    async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
+        print("Database error: %s", exc)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"error": "database_error", "detail": "DB operation failed"},
+        )
+
+    @app.exception_handler(Exception)
+    async def generic_exception_handler(request: Request, exc: Exception):
+        print("Unexpected error: %s", exc)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"error": "internal_server_error", "detail": "Something went wrong"},
+        )
+
+
 # region Token Exceptions
 class BaseTokenException(Exception):
     pass
