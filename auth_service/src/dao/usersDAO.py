877b1b47bf80e94005f8350f8b372c99f868897f
vladbily <vladbilyk2005@gmail.com>
2025-09-19 21:07:18 +0700

    add decorator for funcs

--- auth_service/src/dao/usersDAO.py
+++ auth_service/src/dao/usersDAO.py
@@ -1,5 +1,5 @@
 from sqlalchemy import select
-from sqlalchemy.exc import NoResultFound, SQLAlchemyError
+from sqlalchemy.exc import NoResultFound
 
 from src.dao.baseDAO import BaseDAO
 from src.models.users.users import Users
@@ -9,26 +9,16 @@ class UsersDAO(BaseDAO[Users]):
     def __init__(self):
         super().__init__(model=Users)
 
+    @BaseDAO.with_exception
     async def get_user_by_email(self, email: str) -> Users | None:
-        try:
-            async with self._get_session() as session:
-                stmt = select(self.model).where(self.model.email == email)
-                res = await session.execute(stmt)
-                result = res.scalars().first()
+        async with self._get_session() as session:
+            stmt = select(self.model).where(self.model.email == email)
+            res = await session.execute(stmt)
+            return res.scalars().first()
 
-                return result
-        except SQLAlchemyError as e:
-            raise e
-
+    @BaseDAO.with_exception
     async def set_password(self, user_id: int, hashed_password: str) -> bool:
-        """Обновление пароля пользователя."""
-        try:
-            updated_user = await self.update(user_id, password=hashed_password)
-
-            if not updated_user:
-                raise NoResultFound(f"Пользователь с id={user_id} не найден")
-
-            return True
-
-        except SQLAlchemyError as e:
-            raise e
+        updated_user = await self.update(user_id, password=hashed_password)
+        if not updated_user:
+            raise NoResultFound(f"Пользователь с id={user_id} не найден")
+        return True
