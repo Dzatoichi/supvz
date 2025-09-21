e842fa1f79485e0d02a8047eeac7faa51c15175b
re1kur <re1kur@yahoo.com>
2025-09-21 21:43:00 +0700

    Перевод, установка русских комментариев в auth_service

--- auth_service/src/schemas/tokens_schemas.py
+++ auth_service/src/schemas/tokens_schemas.py
@@ -4,10 +4,16 @@ from pydantic import BaseModel
 
 
 class TokenTypesEnum(StrEnum):
+    """
+    Типы токенов. Эминем.
+    """
     access = "access"
     refresh = "refresh"
 
 
 class TokenSchema(BaseModel):
+    """
+    Схема для токенов.
+    """
     access_token: str
     refresh_token: str
