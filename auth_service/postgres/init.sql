a7e216b8b21fd35694cee41e0249a9cc158ac57e
vladbily <vladbilyk2005@gmail.com>
2025-09-21 18:03:25 +0700

    fix init.sql

--- auth_service/postgres/init.sql
+++ auth_service/postgres/init.sql
@@ -6,4 +6,19 @@ CREATE EXTENSION IF NOT EXISTS pg_cron;
 ALTER SYSTEM SET cron.database_name = 'auth_db';
 SELECT pg_reload_conf();
 
-GRANT USAGE ON SCHEMA cron TO postgres;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM cron.job WHERE jobname = 'delete-expired-tokens'
+    ) THEN
+        PERFORM cron.schedule(
+            'delete-expired-tokens',
+            '0 0 * * 0',
+            'DELETE FROM refresh_tokens WHERE expires_at < NOW()'
+        );
+        RAISE NOTICE 'Cron job for token cleanup created';
+    ELSE
+        RAISE NOTICE 'Cron job already exists';
+    END IF;
+END
+$$;
