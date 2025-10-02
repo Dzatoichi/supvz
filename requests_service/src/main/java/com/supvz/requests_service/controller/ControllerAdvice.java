62d079109d6326a212655f941fb3338971dcb337
re1kur <re1kur@yahoo.com>
2025-10-03 01:55:59 +0700

    done

--- requests_service/src/main/java/com/supvz/requests_service/controller/ControllerAdvice.java
+++ requests_service/src/main/java/com/supvz/requests_service/controller/ControllerAdvice.java
@@ -1,8 +1,12 @@
 package com.supvz.requests_service.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandler {
     // todo
+    public ResponseEntity<?> handle() {
+
+    }
 }
