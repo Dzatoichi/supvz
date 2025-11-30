4b3e3640d755eadcb13e8ac12fd4abc1a803bc3a
re1kur <re1kur@yahoo.com>
2025-11-30 19:27:51 +0700

    todo сделаны

--- notifications_service/src/main/java/com/supvz/notifications_service/config/AppConfig.java
+++ notifications_service/src/main/java/com/supvz/notifications_service/config/AppConfig.java
@@ -2,14 +2,16 @@ package com.supvz.notifications_service.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL;
 
@@ -18,6 +20,20 @@ import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN
 @EnableRetry
 @EnableAsync
 public class AppConfig {
+    @Value("${app.notification.thread.core-pool-size:20}")
+    private Integer notificationCorePoolSize;
+    @Value("${app.notification.thread.max-pool-size:50}")
+    private Integer notificationMaxPoolSize;
+    @Value("${app.notification.thread.queue-capacity:50}")
+    private Integer notificationQueueCapacity;
+    @Value("${app.notification.thread.name-prefix:notification}")
+    private String notificationThreadNamePrefix;
+    @Value("${app.notification.thread.wait-tasks-shutdown:true}")
+    private Boolean notificationWaitTasksShutdown;
+    @Value("${app.notification.await-termination-sec:60}")
+    private Integer notificationAwaitTerminationSeconds;
+
+
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
@@ -27,8 +43,15 @@ public class AppConfig {
 
     @Bean
     public Executor notificationExecutor() {
-        return Executors.newFixedThreadPool(30);
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(notificationCorePoolSize);
+        executor.setMaxPoolSize(notificationMaxPoolSize);
+        executor.setQueueCapacity(notificationQueueCapacity);
+        executor.setThreadNamePrefix(notificationThreadNamePrefix);
+        executor.setWaitForTasksToCompleteOnShutdown(notificationWaitTasksShutdown);
+        executor.setAwaitTerminationSeconds(notificationAwaitTerminationSeconds);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        return executor;
     }
-//    todo: в очередной раз задуматься, какие есть Executor'ы. Может, тут подходит ThreadPoolTaskExecutor?
-//     Если так, то почему?
 }
--- notifications_service/src/main/java/com/supvz/notifications_service/controller/ControllerAdvice.java
+++ notifications_service/src/main/java/com/supvz/notifications_service/controller/ControllerAdvice.java
@@ -1,7 +1,5 @@
 package com.supvz.notifications_service.controller;
 
-import com.supvz.notifications_service.core.exception.InboxEventConflictException;
-import com.supvz.notifications_service.core.exception.InboxEventNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.HttpStatus;
@@ -27,18 +25,4 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
         Map<String, Object> body = Map.of("status", HttpStatus.INTERNAL_SERVER_ERROR, "message", "database is unavailable.");
         return ResponseEntity.internalServerError().body(body);
     }
-
-    @ExceptionHandler(InboxEventConflictException.class)
-    public ResponseEntity<Map<String, Object>> handleInboxEventConflictException(InboxEventConflictException ex) {
-        Map<String, Object> body = Map.of("status", HttpStatus.CONFLICT, "message", ex.getMessage());
-        return ResponseEntity.internalServerError().body(body);
-    }
-
-    @ExceptionHandler(InboxEventNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleInboxEventNotFoundException(InboxEventNotFoundException ex) {
-        Map<String, Object> body = Map.of("status", HttpStatus.BAD_REQUEST, "message", ex.getMessage());
-        return ResponseEntity.internalServerError().body(body);
-    }
-
-//    todo: еще исключения появились
 }
