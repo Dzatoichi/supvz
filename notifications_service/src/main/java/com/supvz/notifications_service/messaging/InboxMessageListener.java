14d6ab199ebca869780fffcfb6c2d0eaa1e0b1ed
re1kur <re1kur@yahoo.com>
2025-12-11 19:49:21 +0700

    trying

--- notifications_service/src/main/java/com/supvz/notifications_service/messaging/InboxMessageListener.java
+++ notifications_service/src/main/java/com/supvz/notifications_service/messaging/InboxMessageListener.java
@@ -6,7 +6,11 @@ import com.supvz.notifications_service.core.exception.InboxEventNotSerializedExc
 import com.supvz.notifications_service.service.initializer.InboxInitializer;
 import com.supvz.notifications_service.model.dto.InboxMessage;
 import com.supvz.notifications_service.model.entity.InboxEventType;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import io.opentelemetry.context.Context;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,8 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -28,6 +34,8 @@ import java.util.stream.Collectors;
 public class InboxMessageListener {
     private final ObjectMapper objectMapper;
     private final Map<InboxEventType, InboxInitializer> initializers;
+    private final Tracer tracer;
+    private final Propagator propagator;
 
     /**
      * Конструктор.
@@ -35,11 +43,15 @@ public class InboxMessageListener {
     @Autowired
     public InboxMessageListener(
             ObjectMapper objectMapper,
-            List<InboxInitializer> initializerList
+            List<InboxInitializer> initializerList,
+            Tracer tracer,
+            Propagator propagator
     ) {
         this.objectMapper = objectMapper;
         this.initializers = initializerList.stream()
                 .collect(Collectors.toMap(InboxInitializer::getType, Function.identity()));
+        this.tracer = tracer;
+        this.propagator = propagator;
     }
 
     /**
@@ -49,8 +61,16 @@ public class InboxMessageListener {
      */
     @RabbitListener(queues = "${app.messaging.inbox-queue}")
     public void listen(Message message) {
+        Map<String, Object> headers = message.getMessageProperties().getHeaders();
+//        Context context = propagator.extract(
+//                Context.current(),
+//                headers,
+//                (header, key) -> String.valueOf(header.get(key))
+//        );
+
         try {
             InboxMessage inboxEvent = objectMapper.readValue(message.getBody(), InboxMessage.class);
+            MDC.put("event_id", inboxEvent.eventId().toString());
             log.debug("Получено из очереди inbox событие [{}].", inboxEvent.eventId());
             try {
                 initializers.get(inboxEvent.eventType()).initialize(inboxEvent);
