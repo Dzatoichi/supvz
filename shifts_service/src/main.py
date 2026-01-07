2cd8f0ec7bef69ff30c2c833fcab8375998d3ab5
vladbily <vladbilyk2005@gmail.com>
2026-01-07 15:06:43 +0700

    feat: update database configuration for testing and add new factories
    - Changed DB_PORT from 5432 to 5433 in .env.test for testing environment.
    - Updated docker-compose.yml to use postgres:17 and added a new postgres_test service for testing.
    - Added new dependencies faker and polyfactory to poetry.lock and pyproject.toml for generating test data.
    - Removed LoggingMiddleware from main.py.
    - Changed scheduled_shift_id type from BigInteger to Integer in shifts.py model.
    - Added field validators to remove timezone info from datetime fields in shifts_schemas.py.
    - Refactored tests to use AsyncPersistenceFactory for creating test data and added integration tests for shift creation, retrieval, update, and deletion.
    - Created new migration script for shifts table.
    - Added run_tests.py script to automate test database setup and execution of pytest.

--- shifts_service/src/main.py
+++ shifts_service/src/main.py
@@ -3,12 +3,9 @@ from fastapi_pagination import add_pagination
 
 from .routers import api_router
 from .utils.exceptions import setup_exception_handlers
-from .utils.middleware import LoggingMiddleware
 
 app = FastAPI()
 setup_exception_handlers(app)
 
-
-app.add_middleware(LoggingMiddleware)
 app.include_router(api_router)
 add_pagination(app)
